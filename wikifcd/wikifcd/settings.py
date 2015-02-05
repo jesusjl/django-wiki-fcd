5e957e674948304711b7afe212629ede77214190
Jesus <gesusjl@gmail.com>
2015-02-05 11:45:40 -0600

    Fixed a problem with TEMPLATE_CONTEXT_PROCESSORS

--- wikifcd/wikifcd/settings.py
+++ wikifcd/wikifcd/settings.py
@@ -47,6 +47,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+	"django.contrib.auth.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.core.context_processors.static",
+	"django.core.context_processors.tz",
+	"django.contrib.messages.context_processors.messages",
+)
+
 ROOT_URLCONF = 'wikifcd.urls'
 
 WSGI_APPLICATION = 'wikifcd.wsgi.application'
